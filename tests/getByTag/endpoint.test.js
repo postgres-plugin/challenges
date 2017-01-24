e91a3b011e5062c598fa4647373f0da51849d4a3
mk4111 <marie.kasai@live.co.uk>
2017-01-24 16:24:48 +0000

    updates tests and fixes inactive chal query bug

--- tests/getByTag/endpoint.test.js
+++ tests/getByTag/endpoint.test.js
@@ -6,7 +6,7 @@ var config = require('../../config/load-config.js');
 
 test('getByTag endpoint', function (t) {
   initServer(config, function (err, server, pool) {
-    var options = { url: '/getByTag?tagId=69' };
+    var options = { url: '/getByTag?tagId=9' };
 
     if (err) {
       return t.fail('Error starting the server, error: ', err);
@@ -14,19 +14,16 @@ test('getByTag endpoint', function (t) {
 
     return server.inject(options, function (response) {
       var filter = response.result.filter;
-      var expectedFilter = { id: 69, name: 'Design for disassembly' };
+      var expectedFilter = { id: 9, name: 'Automotive and Transport Manufacturing' };
       t.deepEqual(filter, expectedFilter, 'the filter tag returns the correct object');
 
       var challenges = response.result.challenges;
 
-      t.equal(challenges.length, 2,
-        'Two challenges returned when filtering by tag with id 69');
+      t.equal(challenges.length, 1,
+        'Only one challenge returns when filtering by tag with id 9');
 
-      t.equal(challenges[0].tags.length, 4,
-        'First challenge has 4 tags attached');
-
-      t.equal(challenges[1].tags.length, 1,
-        'Two challenges returned when filtering by tag with id 69');
+      t.ok(challenges[0].title.indexOf('Challenge Number 4') > -1,
+        'Title of the active challenge in returned correctly');
 
       return pool.end(function () { // eslint-disable-line
         server.stop(t.end);
@@ -34,7 +31,6 @@ test('getByTag endpoint', function (t) {
     });
   });
 });
-
 
 test('getByTag function when there is no match', function (t) {
   initServer(config, function (err, server, pool) {
--- tests/getByTag/query.test.js
+++ tests/getByTag/query.test.js
@@ -3,46 +3,47 @@
 var test = require('tape');
 var getByTag = require('../../lib/queries/getByTag.js');
 
-var query = 'SELECT '
-+ 'tags_challenges.tags_id as tid, '
-+ 'tags.name AS tags_name, '
-+ 'challenges.id AS chal_id, '
-+ 'challenges.date AS chal_date, '
-+ 'challenges.title AS chal_title, '
-+ 'challenges.description AS chal_description, '
-+ 'organisations.name AS org_name, '
-+ 'organisations.id AS org_id, '
-+ '68 AS filter_id, '
-+ '(SELECT tags.name WHERE tags.id = 68) AS filter_name '
-+ 'FROM tags_challenges '
-+ 'JOIN '
-+ 'challenges '
-+ 'ON '
-+ 'tags_challenges.challenges_id = challenges.id '
-+ 'RIGHT OUTER JOIN '
-+ 'tags '
-+ 'ON '
-+ 'tags_challenges.tags_id = tags.id '
-+ 'LEFT OUTER JOIN '
-+ 'organisations '
-+ 'ON '
-+ 'challenges.org_id = organisations.id '
-+ 'WHERE tags_challenges.challenges_id '
-+ 'IN ( '
-+ '  SELECT '
-+ '  challenges_id as chal_id '
-+ '  FROM '
-+ '  tags_challenges '
-+ '  WHERE '
-+ '  tags_challenges.tags_id = 68 '
-+ ') '
-+ 'OR tags.id = 68 '
-+ 'AND tags.active IS NOT FALSE '
-+ 'AND challenges.active IS NOT FALSE '
-+ 'AND organisations.active IS NOT FALSE '
-+ 'ORDER BY challenges.date DESC, '
-+ 'challenges.title ASC;';
-
+var query = [
+  'SELECT',
+  'filter_id, filter_name, chal_id, chal_date, chal_title, chal_description, org_id, org_name',
+  'FROM (',
+    'SELECT',
+    '68 AS filter_id,',
+    'tags.name AS filter_name',
+    'FROM',
+    'tags WHERE tags.id = 68',
+  ') AS filter_details',
+  'LEFT OUTER JOIN (',
+    'SELECT',
+      'tid, chal_id, chal_date, chal_title, chal_description, org_id, org_name',
+      'FROM (',
+      'SELECT',
+      'tags_id AS tid,',
+      'challenges_id AS chal_id',
+      'FROM tags_challenges',
+    ') AS challenge_ids',
+    'INNER JOIN (',
+      'SELECT',
+      'id, org_id,',
+      'date AS chal_date,',
+      'title AS chal_title,',
+      'description AS chal_description',
+      'FROM challenges',
+      'WHERE challenges.active is not false',
+    ') AS challenge_details',
+    'on challenge_details.id = chal_id',
+    'INNER JOIN (',
+      'SELECT',
+      'id, name AS org_name',
+      'FROM organisations',
+      'WHERE organisations.active is not false',
+    ') AS org_details',
+    'on org_details.id = org_id',
+  ') AS challenges_all',
+  'on challenges_all.tid = filter_details.filter_id',
+  'ORDER BY chal_date DESC,',
+  'chal_title ASC;'
+].join(' ');
 
 test('getByTag query string', function (t) {
   t.equal(getByTag(68), query,
