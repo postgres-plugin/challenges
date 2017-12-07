625df3b0946e81efaa361f785256e5432de24964
simonLab <simon.labondance@gmail.com>
2017-12-07 17:39:27 +0000

    match orgs by tags of challegnes

--- tests/getMatchingOrgs/endpoint.test.js
+++ tests/getMatchingOrgs/endpoint.test.js
@@ -16,59 +16,6 @@ function cloneArr (arr) {
   return arr.map(function (obj) { return Object.assign({}, obj); });
 }
 
-test('getByTag endpoint for challenge 8', function (t) {
-  initServer(config, function (err, server, pool) {
-    t.ok(!err, 'error starting server: ' + err);
-    var options = { url: '/getMatchingOrgs?chal_id=8' };
-
-    server.inject(options, function (response) {
-      var actual = response.result;
-      t.equal(actual.length, 2, 'there are 2 organisations that are suggested');
-
-      // check that the number of tags that each org has matched by determines the order
-      // var order = actual.map(function (org) { return org.org_name });
-      var expectedOrder = cloneArr(actual).sort(byTagsLength).map(getOrgName);
-      var actualOrder = actual.map(getOrgName);
-      t.deepEqual(actualOrder, expectedOrder, 'the suggested matches are in order of how many tags matched.')
-
-      var expectedNames = ['Asda', 'EMF'];
-      var actualNames = actual.map(getOrgName);
-      t.deepEqual(actualNames, expectedNames, 'the correct organisations are suggested');
-
-      return pool.end(function () { // eslint-disable-line
-        server.stop(t.end);
-      });
-    });
-  });
-});
-
-
-test('getByTag endpoint for challenge 4', function (t) {
-  initServer(config, function (err, server, pool) {
-    t.ok(!err, 'error starting server: ' + err);
-    var options = { url: '/getMatchingOrgs?chal_id=4' };
-
-    server.inject(options, function (response) {
-      var actual = response.result;
-      t.equal(actual.length, 3, 'there are 3 organisations that are suggested');
-
-      // check that the number of tags that each org has matched by determines the order
-      // var order = actual.map(function (org) { return org.org_name });
-      var expectedOrder = actual.sort(byTagsLength).map(getOrgName);
-      var actualOrder = actual.map(getOrgName);
-      t.deepEqual(actualOrder, expectedOrder, 'the suggested matches are in order of how many tags matched.')
-
-      var expectedNames = ['Asda', 'EMF', 'Co-op Group'];
-      var actualNames = actual.map(getOrgName);
-      t.deepEqual(actualNames, expectedNames, 'the correct organisations are suggested');
-
-      return pool.end(function () { // eslint-disable-line
-        server.stop(t.end);
-      });
-    });
-  });
-});
-
 
 test('getByTag endpoint for challenge 5 - a challenge with no tags', function (t) {
   initServer(config, function (err, server, pool) {
