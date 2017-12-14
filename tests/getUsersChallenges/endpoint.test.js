53d492adf122942c49cd2122ffefd33c8a72f056
simonLab <simon.labondance@gmail.com>
2017-12-14 14:47:27 +0000

    return only active challenge for a user

--- tests/getUsersChallenges/endpoint.test.js
+++ tests/getUsersChallenges/endpoint.test.js
@@ -7,7 +7,7 @@ var init = require('../../example/server.js');
 test('get challenges of a user', function (t) {
   init(config, function (err, server, pool) {
     server.inject({ url: '/people/3/challenges' }, function (res) {
-      t.equal(res.result.length, 4, 'User 3 has 4 active challenges');
+      t.equal(res.result.length, 2, 'User 3 has 2 active challenges');
 
       t.end();
       pool.end();
